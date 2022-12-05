e1b558b6502410f07c7526941716382b55a51818
Muskan Valmikee <muskanvalmikee@gmail.com>
2022-12-05 22:42:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import React, { useState } from "react"
+import './App.css'
 const _ =require('lodash')
 
-function App() {
+export default function App() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
+    const [email, setEmail] = useState("") 
+    const [password, setPassword] = useState("")
     const [allData, setAllData] = useState([])
-    const [searchItem,setSearchItem] =useState("")
+    const [searchData,setsearchData] =useState("")
     const [filteredData,setFilteredData]=useState([])
-
     const searchFunction=()=>{
-        if(searchItem.length >3){
-            let filteredItems = _.filter(allData, {"firstName":searchItem})
-            setFilteredData(filteredItems)
-            console.log(filteredItems)
+        if(searchData.length >3){
+            let filterDataItems = _.filter(allData, {"firstName":searchData})
+            setFilteredData(filterDataItems)
         }
-    }
+    }      
     return (
-        <div>
-            <div>
-                <input type="text" placeholder="Search" onChange={(e)=>{
-                    setSearchItem(e.target.value)
-                    searchFunction()
-                }}/>
-            </div>
-            <input type="text" placeholder="Enter first name" onChange={(e) => {
+        
+        <div id="maindiv">
+            <center>
+            <h1>User Data & Search Bar</h1>
+            <input type="text" placeholder="First Name" onChange={(e) => {
                 setFirstName(e.target.value)
             }} />
-            <input type="text" placeholder="Enter last name" onChange={(e) => {
+            <input type="text" placeholder="Last Name" onChange={(e) => {
                 setLastName(e.target.value)
             }} />
-            <input type="text" placeholder="Enter  email" onChange={(e) => {
+            <input type="email" placeholder="Email" onChange={(e) => {
                 setEmail(e.target.value)
             }} />
-            <input type="password" placeholder="Enter password" onChange={(e) => {
+            <input type="password" placeholder="Password" onChange={(e) => {
                 setPassword(e.target.value)
             }} />
+
             <button onClick={() => {
 
                 let tempObj = {
@@ -46,44 +43,57 @@ function App() {
                     password: password
                 }
                 setAllData([...allData,tempObj])
-            }}>Submit</button>
+            }}>Signup</button>
+            <div>
+                <input type="text" placeholder="Search..." onChange={(e)=>{
+                    setsearchData(e.target.value)
+                    searchFunction()
+                }}/>
+            </div>
+            </center>
             {
-                searchItem.length>3?filteredData.map((item)=>{
+                searchData.length>3?filteredData.map((item,index)=>{
                     return(
                         <div>
-                            <h1>{item.firstName}</h1>
-                            <h1>{item.lastName}</h1>
-                        </div>
-                    )
-                }) :allData.map((item)=>{
-                    return(
-                        <div>
-                            <h1>{item.firstName}</h1>
-                            <h1>{item.lastName}</h1>
-                        </div>
-                    )
-                })
-        }
-
-{
-            allData.map((item)=>{
-                return(
-                    <div>
-                    <table>
-                        <tr>
+                            <center>
+                            <table>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Email</th>
+                                <th>Password</th>
+                                <tr>
                             <td>{item.firstName}</td>
                             <td>{item.lastName}</td>
                             <td>{item.email}</td>
                             <td>{item.password}</td>
-                        </tr>
-                    </table>
-                    </div>
-                )
-            })
+                            </tr> 
+                            </table>
+                            </center>
+                        </div>
+                    )
+                }) :allData.map((item,index)=>{
+                    return(
+                        <div>
+                            <center>
+                            <table>
+                            <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Email</th>
+                                <th>Password</th>
+                                <tr>
+                            <td>{item.firstName}</td>
+                            <td>{item.lastName}</td>
+                            <td>{item.email}</td>
+                            <td>{item.password}</td>
+                            </tr>
+                            </table>
+                            </center>
+                        </div>
+                    )
+                })
+            
         }
+        </div>  
 
-        </div>
-    )
+    ) 
 }
-
-export default App;
